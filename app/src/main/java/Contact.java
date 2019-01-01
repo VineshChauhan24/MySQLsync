2e2844c344ee799bc6589462ab64e076d0eca4df
Dennis <dmnickolai@gmail.com>
2019-01-01 13:18:50 -0800

    Updated UI layout files Add Contact class

---
+++ app/src/main/java/Contact.java
@@ -0,0 +1,12 @@
+public class Contact {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private int syncStatus;
+}
--- app/src/main/java/com/palibre/mysqlsync/MainActivity.java
+++ app/src/main/java/com/palibre/mysqlsync/MainActivity.java
@@ -1,13 +1,19 @@
 package com.palibre.mysqlsync;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    SQLiteDatabase ourDB = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addNew (View v) {
+
     }
 }

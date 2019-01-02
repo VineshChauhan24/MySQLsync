f6623f440c089a235925556dc7169fec81aedca4
Dennis <dmnickolai@gmail.com>
2019-01-02 09:33:01 -0800

    Add Button Event code

--- app/src/main/java/com/palibre/mysqlsync/MainActivity.java
+++ app/src/main/java/com/palibre/mysqlsync/MainActivity.java
@@ -4,16 +4,26 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SQLiteDatabase ourDB = null;
+    TextView txtName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtName = findViewById(R.id.new_name);
+
     }
 
     public void addNew (View v) {
-
+        String name = txtName.getText().toString();
+        if (name.isEmpty() ) {
+            Toast.makeText(this, "New name is empty", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(this, "Adding " + name + " to database", Toast.LENGTH_LONG).show();
     }
 }
